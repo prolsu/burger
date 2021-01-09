7b737471afa4bb5b336200d8713dc42db2a0765e
Pedro Alburqueque <prolsu88@gmail.com>
2021-01-09 12:51:31 -0500

    set up schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
-DROP DATABASE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS o2wqlmo7ejz73osx;
 
-CREATE DATABASE burgers_db;
+CREATE DATABASE o2wqlmo7ejz73osx;
 
-USE burgers_db;
+USE o2wqlmo7ejz73osx;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(20) NULL,
     devoured BOOLEAN,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
